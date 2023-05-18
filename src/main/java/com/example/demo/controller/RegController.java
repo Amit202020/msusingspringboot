d2963f99c9b272fb9094f87326bd8f4d3a55f175
Amitava Kundu <amitava.kundu@atos.net>
2023-05-18 15:29:09 +0530

    class free code

---
+++ src/main/java/com/example/demo/controller/RegController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.bean.Employee;
+
+@RestController
+public class RegController {
+
+	@GetMapping("/allEmp")
+	public List<Employee> getAllEmp() {
+
+		List<Employee> empList = Arrays.asList(new Employee(1001, "Srini", "dev"), new Employee(1002, "Srini", "dev"),
+				new Employee(1003, "Srini", "dev")
+
+		);
+		return empList;
+
+	}
+
+}
